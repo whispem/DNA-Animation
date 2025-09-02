8fefcc906af3d16c4bef8f90ae7842fcc3154a8d
Em <contact.whispem@gmail.com>
2025-09-02 12:29:24 +0200

    Add files via upload

---
+++ DNA Animation/DNA Animation/DNA_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DNA_AnimationApp.swift
+//  DNA Animation
+//
+//  Created by Emilie on 01/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct DNA_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
---
+++ DNA Animation/DNA AnimationTests/DNA_AnimationTests.swift
@@ -0,0 +1,17 @@
+//
+//  DNA_AnimationTests.swift
+//  DNA AnimationTests
+//
+//  Created by Emilie on 01/09/2025.
+//
+
+import Testing
+@testable import DNA_Animation
+
+struct DNA_AnimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
